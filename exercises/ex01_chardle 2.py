a63acb0bf38470bca5c231d4ae1739bdd60feac5
Heidi Sipes <hsipes@unc.edu>
2022-01-22 14:36:56 -0500

    working

--- exercises/ex01_chardle 2.py
+++ exercises/ex01_chardle 2.py
@@ -3,7 +3,7 @@
 __author__ = "730469262"
 
 
-word: str = input("Enter a five character word:")
+word: str = input("Enter a five letter word:")
 if len(word) < 5 or len(word) > 5:
     print("Error: Word must contain 5 characters")
     exit()
@@ -14,15 +14,18 @@ if len(letter) > 1:
 print("Searching for " + letter + " in " + word)
 
 if word[0] == letter:
-    print(letter, "is at index 0")
-if word[1] == letter:   
+    print(letter, "is at index 0") 
+elif word[1] == letter:   
     print(letter, "is at index 1")
-if word[2] == letter:
+elif word[2] == letter:
     print(letter, "is at index 2")
-if word[3] == letter:
+elif word[3] == letter:
     print(letter, "is at index 3")
-if word[4] == letter:
+elif word[4] == letter:
     print(letter, "is at index 4")
+else:
+    print("no letter found")
+
 
 count: int = word.count(letter)
 
@@ -33,3 +36,5 @@ else:
         print("1 instance of", letter, "found in", word)
     if count == 2 or count == 3 or count == 4 or count == 5:
         print(count, "instances of", letter, "found in", word)
+
+    
