a23e648ff8e44ebac3b0f9777bd714344732dda5
Heidi Sipes <hsipes@unc.edu>
2022-03-12 21:42:29 -0500

    changes

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -8,18 +8,10 @@ __author__ = "730469262"
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """Read the rows  of a csv into a 'table'."""
     result: list[dict[str, str]] = []
-    
-    # Open a handle to the data file
     file_handle = open(filename, "r", encoding="utf8")
-   
-    # Prepare to read the data file as a CSV rather than just strings
     csv_reader = DictReader(file_handle)
-
-    # Read each row of the CSV line-by-line
     for row in csv_reader:
         result.append(row)
-
-    # Close the file when we're done, to free its resources.
     file_handle.close()
     return result
 
